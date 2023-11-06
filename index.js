e64c8b4ad98f1ebcf32c1efa8885f281c768f4f5
nadira1187 <nadira35-1187@s.diu.edu.bd>
2023-11-06 17:25:39 +0600

    Remove node_modules from Git tracking

--- index.js
+++ index.js
@@ -2,8 +2,8 @@ const express =require('express');
 const app = express();
 const cors =require('cors');
 require('dotenv').config();
-//const jwt = require('jsonwebtoken');
-//const cookieParser=require('cookie-parser');
+const jwt = require('jsonwebtoken');
+const cookieParser=require('cookie-parser');
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 
 
@@ -11,14 +11,11 @@ const port =process.env.PORT ||5000;
 
 //middleware 
 app.use(cors({
-    origin:[
-      'http://localhost:5173'
-    ],
-    optionsSuccessStatus: 200,
+    origin: 'http://localhost:5173',
     credentials:true
 }));
 app.use(express.json());
-//app.use(cookieParser());
+app.use(cookieParser());
 
 //middlewareno
 
@@ -35,89 +32,93 @@ const client = new MongoClient(uri, {
     deprecationErrors: true,
   }
 });
-// const logger =async(req,res,next)=>{
-//    // console.log('called',req.host,req.originalUrl)
-//     next();
-// }
-// const verifyToken =async(req,res,next)=>{
-//     const token=req.cookies?.token;
-//     //console.log('value of token',token)
-//     if(!token)
-//     {
-//         return res.status(401).send({message:'not authorized'});
-//     }
-//     jwt.verify(token,process.env.ACCESS_TOKEN_SECRET,(err,decoded)=>{
-//         if(err){
-//             return res.status(401).send({message:'unauthorized'});
-//         }
-//         //console.log('value of token',decoded)
-//         req.user=decoded;
-//         next();
-//     })
+const logger =async(req,res,next)=>{
+   // console.log('called',req.host,req.originalUrl)
+    next();
+}
+const verifyToken =async(req,res,next)=>{
+    const token=req.cookies?.token;
+   // console.log('value of token',token)
+    if(!token)
+    {
+        return res.status(401).send({message:'not authorized'});
+    }
+    jwt.verify(token,process.env.ACCESS_TOKEN_SECRET,(err,decoded)=>{
+        if(err){
+            return res.status(401).send({message:'unauthorized'});
+        }
+       // console.log('value of token',decoded)
+        req.user=decoded;
+        next();
+    })
   
-// }
+}
 
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    //await client.connect();
+    await client.connect();
     const roomsCollection=client.db('roomDb').collection('rooms');
-    //const bookingCollection =client.db('roomDb').collection('bookings');
+    const bookingCollection =client.db('roomDb').collection('bookings');
     //const reviewCollection=client.db('roomDb').collection('reviews');
-    // app.post('/jwt',logger,async(req,res)=>{
-    //     const user=req.body;
-    //     console.log(user);
-    //     const token =jwt.sign(user,process.env.ACCESS_TOKEN_SECRET,{expiresIn:'1h'})
-    //     res
-    //     .cookie('token',token,{
-    //         httpOnly:true,
-    //         secure:false,
-    //         sameSite:'none',
+    app.post('/jwt',logger,async(req,res)=>{
+        const user=req.body;
+        console.log(user);
+        const token =jwt.sign(user,process.env.ACCESS_TOKEN_SECRET,{expiresIn:'1h'})
+        res
+        .cookie('token',token,{
+            httpOnly:true,
+            secure:false,
+            //sameSite:'none',
+        })
+        .send({success:true})
+    })
+
+
+    //  app.post('/logout', async (req, res) => {
+    //         const user = req.body;
+    //         console.log('logging out', user);
+    //         res.clearCookie('token', { maxAge: 0 }).send({ success: true })
     //     })
-    //     .send({success:true})
-    // })
     app.get('/rooms',async(req,res) =>{
         const cursor =roomsCollection.find();
         const result= await cursor.toArray();
         res.send(result);
     })
-    //  app.get('/service/:id',async(req,res)=>{
-    //     const id=req.params.id;
-    //     const query ={_id:new ObjectId(id)}
-    //     const options ={
-    //         projection:{title:1, price:1,service_id:1,img:1}
-    //     }
-    //     const result = await serviceCollection.findOne(query ,options);
-    //     res.send(result);
-    //  })
-    //  app.get('/booking',logger,verifyToken,async(req,res)=>{
-    //     console.log(req.query.email);
-    //    // console.log('tok tok token ',req.cookies.token)
-    //    //console.log('from valid token',req.user)
-    //    if(req.query.email!==req.user.email)
-    //    {
-    //     return res.status(403).send({message:'forbidden access'})
-    //    }
-    //     let query ={};
-    //     if(req.query?.email)
-    //     {
-    //         query={ email:req.query.email }
-    //     }
-    //     const result =await bookingCollection.find(query).toArray();
-    //     res.send(result)
-    //  })
-    //  app.post('/bookings',async(req,res)=>{
-    //     const booking=req.body;
-    //     //console.log(booking);
-    //     const result =await bookingCollection.insertOne(booking);
-    //     res.send(result);
-    //  })
-    //  app.delete('/bookingss/:id',async(req,res)=>{
-    //     const id=req.params.id;
-    //     const query={_id:new ObjectId(id)}
-    //     const result=await bookingCollection.deleteOne(query);
-    //     res.send(result);
-    //  })
+     app.get('/rooms/:id',async(req,res)=>{
+        const id=req.params.id;
+        const query ={_id:new ObjectId(id)}
+        const result = await roomsCollection.findOne(query );
+        res.send(result);
+     })
+     app.get('/booking',logger,verifyToken,async(req,res)=>{
+        // console.log(req.query.email);
+        //  console.log('tok tok token ',req.cookies.token)
+        //  console.log('from valid token',req.user)
+       if(req.query.email!==req.user.email)
+       {
+        return res.status(403).send({message:'forbidden access'})
+       }
+        let query ={};
+        if(req.query?.email)
+        {
+            query={ email:req.query.email }
+        }
+        const result =await bookingCollection.find(query).toArray();
+        res.send(result)
+     })
+     app.post('/bookings',async(req,res)=>{
+        const booking=req.body;
+        //console.log(booking);
+        const result =await bookingCollection.insertOne(booking);
+        res.send(result);
+     })
+     app.delete('/bookingss/:id',async(req,res)=>{
+        const id=req.params.id;
+        const query={_id:new ObjectId(id)}
+        const result=await bookingCollection.deleteOne(query);
+        res.send(result);
+     })
 
 
     // Send a ping to confirm a successful connection
